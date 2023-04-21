b0a0e02a895ad861174350f9df719d9ee8e5518a
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-04-21 14:19:49 +0900

    feat: 스프링 컨테이너 통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,47 +2,40 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		//Spring Container
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// Tomcat, Jetty .. 등을 추상화 해 놓음
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					//servlet mapping -> controller로 전환
+					servletContext.addServlet("dispatcherServlet",
+									//자기 자신을 참고하기에 this를 입력
+									new DispatcherServlet(this))
+							//URL의 매핑
+							.addMapping("/*");
+
+				});
+				//TomcatServlet Container 동작
+				webServer.start();
+			}
+		};
+
 		//Bean 등록::클래스 정보 넘기기
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 
-		// Tomcat, Jetty .. 등을 추상화 해 놓음
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			//servlet mapping -> controller로 전환
-			servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-							)
-					//URL의 매핑
-					.addMapping("/*");
-
-		});
-		//TomcatServlet Container 동작
-		webServer.start();
 	}
 
 }
