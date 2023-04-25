d1bf0e46f85a2eb4c74db47ac633ae0c15cd5d27
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-04-26 07:51:17 +0900

    feat: meta annotation과 compse annotation explain

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,11 +3,27 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest { }
+
+@Retention(RetentionPolicy.RUNTIME)
+//Annotation type도 타겟으로 적용 가능
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest { }
 
 class HelloServiceTest {
 
-    @Test
+    @FastUnitTest
     void sayHello() {
         SimpleHelloService helloService = new SimpleHelloService();
         String result = helloService.sayHello("Spring");
